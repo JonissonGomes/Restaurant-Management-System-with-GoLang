3791c4eead4f6e45de1a0d743ecb70a71f8070c8
JonissonGomes <joni_gomes@hotmail.com>
2022-06-15 14:12:08 -0300

    Criando entidade e atribuindo seus consumos aos endpoints

--- package/entities/restaurant.entity.go
+++ package/entities/restaurant.entity.go
@@ -1,1 +1,46 @@
 package entities
+
+import (
+	"github.com/JonissonGomes/Restaurant-Management-System-with-GoLang/package/config"
+	"github.com/jinzhu/gorm"
+)
+
+var db *gorm.DB
+
+type Restaurant struct {
+	gorm.Model
+	Name          string `json:"name"`
+	Address       string `json:"address"`
+	Phone         string `json:"phone"`
+	Instragram    string `json:"instagram"`
+	QuantityStars int    `json:"quantityStars"`
+}
+
+func init() {
+	config.ConnectDatabase()
+	db = config.GetDB()
+	db.AutoMigrate(&Restaurant{})
+}
+
+func (r *Restaurant) CreateRestaurant() *Restaurant {
+	db.NewRecord(r)
+	db.Create(&r)
+	return r
+}
+
+func GetAllRestaurant() []Restaurant {
+	var restaurants []Restaurant
+	db.Find(&restaurants)
+	return restaurants
+}
+
+func GetRestaurantById(id int64) (*Restaurant, *gorm.DB) {
+	var restaurant Restaurant
+	db := db.Where("id = ?", id).Find(&restaurant)
+	return &restaurant, db
+}
+
+func DeleteRestaurant(id int64) *gorm.DB {
+	db := db.Where("id = ?", id).Delete(&Restaurant{})
+	return db
+}
