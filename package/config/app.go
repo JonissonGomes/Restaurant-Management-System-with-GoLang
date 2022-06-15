7080747cc8e175d5da33bc502c634d832297d4cf
JonissonGomes <joni_gomes@hotmail.com>
2022-06-15 12:50:20 -0300

    Iniciando conexão com o banco

--- package/config/app.go
+++ package/config/app.go
@@ -1,1 +1,20 @@
 package config
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+var DB *gorm.DB
+
+func connectDB() {
+	db, err := gorm.Open("postgres", "host=localhost port=5432 user=postgres dbname=restaurant password=postgres sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+	DB = db
+}
+
+func GetDatabase() *gorm.DB {
+	return DB
+}
